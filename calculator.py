3347375eef6a02a6323acfe84fb536cf9b0b5e07
Sergio Hernandez-Acosta <79073281+sergiojhernandezacosta@users.noreply.github.com>
2023-03-24 13:34:00 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,11 +3,11 @@ import pandas as pd
 import os
 
 # Set up name of new printed Excel report
-project_name = input("Project Name: ")
-axis_name = input("Axis Name: ")
+project_name = "BRENTON 4482"
+axis_name = "LOADER"
 
 # Pandas Read
-location = r"I:\MECH\SHA\Analyzer Selection.xlsx"
+location = r"C:\Users\shacosta\Desktop\Analyzer Selection2.xlsx"
 df = pd.read_excel(location)
 
 # Read Gearbox Specs from Excel Database 
@@ -20,7 +20,7 @@ gbx_torque = pd.to_numeric(df.iloc[1:,5], errors='coerce')
 gbx_cost = pd.to_numeric(df.iloc[1:,6], errors='coerce')
 
 # Inputs
-gbx_type = input("\nSelect type of gearbox: \n NPL \n Cone Drive \n NVH \n VDH \n VH+ \n\n SELECT: ")
+gbx_type = "NPL"
 
 if gbx_type == "Cone Drive":
     first_three_letters = "S"
@@ -35,8 +35,13 @@ if len(selected_gbx_names) == 0:
     print("No gearbox names found for the selected type.")
 else:
     print(selected_gbx_names)
-    
-peak_speed = float(input("\nPeak Speed: "))
+
+
+# Input Calculations from Motion Analyzer 
+peak_speed = 280.74
+peak_torque_input = 66.65
+peak_acceleration = 102.83
+max_inertia = 0.49
 
 
 # Calculate the new speed for each selected gearbox
@@ -52,26 +57,34 @@ servo_peak_torque = pd.to_numeric(df.iloc[1:,12], errors='coerce').dropna()
 servo_velocity = pd.to_numeric(df.iloc[1:,13], errors='coerce').dropna()
 servo_cost = pd.to_numeric(df.iloc[1:,15], errors='coerce').dropna()
 
+seen_combinations = set()
 # Create a list of dictionaries with the desired columns
 results = []
 for name, ratio in zip(selected_gbx_names, gbx_ratio[gbx_name.str.startswith(first_three_letters)]):
     new_speed = ratio * peak_speed
-    for servo, velocity, cost in zip(servo_name, servo_velocity, servo_cost):
-        percentage = new_speed / velocity * 100
-        if percentage <= 80:
-            total_cost = gbx_cost[gbx_name == name].iloc[0] + cost
-            result = {"Servo": servo, "Gearbox Name": name, "Percentage": percentage, "Total Cost": total_cost}
-            results.append(result)
+    for servo, velocity, cost, peak_torque, cont_torque in zip(servo_name, servo_velocity, servo_cost, servo_peak_torque, servo_cont_torque):
+        # Calculations 
+        post_gbx_torque = (peak_torque_input) / ((ratio) * ((1 - ((max_inertia* peak_acceleration) / (peak_torque * ratio)))))
+        percentage_speed = (new_speed / velocity) * 100
+        percentage_peak_torque = (post_gbx_torque / peak_torque) * 100
+        percentage_cont_torque = (post_gbx_torque / cont_torque) * 100
+        
+        gbx_speed_protect = ( (peak_speed  * ratio) / gbx_speed) * 100
+        gbx_torque_protect = (peak_torque_input / gbx_torque) * 100
+
+        combination = None
+        
+        mask = (percentage_speed <= 80) & (percentage_peak_torque <= 80) & (percentage_cont_torque <= 65) & (gbx_speed_protect <= 80) & (gbx_torque_protect <= 80) & (percentage_peak_torque >= 0) & (percentage_cont_torque >= 0)
+        if mask.any():
+            combination = (percentage_speed, percentage_peak_torque, percentage_cont_torque)        
+            
+            if combination is not None and combination not in seen_combinations:
+                total_cost = cost + gbx_cost[gbx_name == name].iloc[0]
+                seen_combinations.add(combination)
+                result = {"Servo": servo, "Gearbox Name": name, "Percentage Speed": percentage_speed, "Percentage Peak Torque": percentage_peak_torque, "Percentage Cont Torque": percentage_cont_torque, "Total Cost": total_cost}
+                results.append(result)
 
 # Convert the list of dictionaries to a pandas dataframe
 df_results = pd.DataFrame(results)
 
-# Export the dataframe to Excel
-file_name = f"{project_name}.xlsx"
-sheet_name = axis_name
-df_results.to_excel(file_name, sheet_name=sheet_name, index=False)
-
-# Set the location of the export
-export_location = r"C:\Users\shacosta\Desktop"
-file_path = os.path.join(export_location, file_name)
-df_results.to_excel(file_path, sheet_name=sheet_name, index=False)
+print(df_results)
